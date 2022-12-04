12deec7237309d8ed540eba127d2dbc276d3e207
Elvin Garcia <elvingarcia19@gmail.com>
2022-12-04 17:24:14 -0500

    users now have the option to change an address on attractions they created and submitted to the database

--- controllers/posts.js
+++ controllers/posts.js
@@ -44,15 +44,48 @@ module.exports = {
   // When a user wants to update an attraction they've created
   updateLocalAttraction: async (req, res) => {
 
-    console.log(req.body)
+    // We need to figure out if a user wants to update the location on an attraction they submitted to the database.
+    // Let's try to compare the Address property in the MongoDB document vs the req.body.address that's coming in
+
     try {
-      await Post.findOneAndUpdate( // Go into the database, find an attraction that matches this ID and update it. 
-        { _id: req.params.id },
-        {
-          $set: req.body // Once founded, just update any matching fileds
-        }
-      );
+
+      // Bring in the attraction from MongoDB so we can compare it to the form the user submitted
+      const local = await Post.findById(req.params.id)
+      
+      // If the address or zip code from the MongoDB stored documnet have a different value than the address coming in from the form
+      // We need to update the address and GPS coordinates so we can send the correct points to our map
+      if(req.body.Address != local.Address || req.body.Zipcode != local.Zipcode) {
+        
+        // If the address has been changed, we need to get the GPS coordinates of the new location
+        // In order to have a map display the location of an attraction we need the longitude and latitude. This api lets us enter an address and gives us back information like longitude and latitude
+        const response = await fetch(`https://api.geoapify.com/v1/geocode/search?text=${req.body.Address}%20${req.body.Zipcode}&format=json&apiKey=1b62ec9563b9425db6840fa43b228361`);
+        let obj = await response.json();
+
+        // Save coordinates received from api call so we can add it to our document which will be saved in our database
+        let longitude = obj.results[0].lon
+        let latitude = obj.results[0].lat
+
+        // Once we have the coordinates of the new address we can update the document in MongoDB
+        await Post.findOneAndUpdate( // Go into the database, find an attraction that matches this ID and update it. 
+          { _id: req.params.id },
+          {
+            $set: req.body, // Once founded, just update any matching fileds
+            Longitude: longitude, // Also update the new Longitude
+            Latitude: latitude // Also update the new latitude
+          }
+        );
+
+      }else { // The user does not need to update the address, so gps coordinates can stay the same.
+
+        await Post.findOneAndUpdate( // Go into the database, find an attraction that matches this ID and update it. 
+          { _id: req.params.id },
+          {
+              set: req.body // Once founded, just update any matching fileds
+          }
+        );
+      }
       res.redirect(`/addAttraction`);
+
     } catch (err) {
       console.log(err);
     }
@@ -68,11 +101,7 @@ module.exports = {
     try {
       const posts = await Comment.find({ user: req.user.id, Complete: "false" }); // user: req.user.id will only show the attractions that the user has saved. On the profile page, we only want to show attractions that have not been completed
 
-      // For now go into the database, see if the logged in user has created any attractions for others to visit.
-      const local = await Post.find({user: req.user.id})
-
-
-      res.render("profile.ejs", { posts: posts, user: req.user, local: local });
+      res.render("profile.ejs", { posts: posts, user: req.user });
     } catch (err) {
       console.log(err);
     }
@@ -110,21 +139,15 @@ module.exports = {
   // Users know their cities the best. They can create an attraction. Whoever enteres a matching zipcode will get attractions added by users
   createPost: async (req, res) => {
 
-    var longitude;
-    var latitude;
-
     try {
       
-
-      let obj;
-
       // In order to have a map display the location of an attraction we need the longitude and latitude. This api lets us enter an address and gives us back information like longitude and latitude
       const response = await fetch(`https://api.geoapify.com/v1/geocode/search?text=${req.body.address}%20${req.body.zipcode}&format=json&apiKey=1b62ec9563b9425db6840fa43b228361`);
-      obj = await response.json();
+      let obj = await response.json();
 
       // Save coordinates received from api call so we can add it to our document which will be saved in our database
-      longitude = obj.results[0].lon
-      latitude = obj.results[0].lat
+      let longitude = obj.results[0].lon
+      let latitude = obj.results[0].lat
       
 
       // Upload image to cloudinary
@@ -185,13 +208,10 @@ module.exports = {
   },
   deletePost: async (req, res) => {
 
-    console.log("Here")
-
     try {
-      // Find post by id
 
+      // Find post by id
       let post = await Comment.findById({ _id: req.params.id });
-      console.log(post)
 
       // Delete image from cloudinary. Not all attractions will have a cloudinary id. Those bookmarked from travel-advisor dont. 
       let cloud = await cloudinary
@@ -201,7 +221,6 @@ module.exports = {
         cloud.uploader.destroy(post.cloudinaryId); // This deletes it from cloudinary becuase we no longer need it
       }
       
-
       // Delete post from db
       await Comment.remove({ _id: req.params.id });
       res.redirect("/profile");
